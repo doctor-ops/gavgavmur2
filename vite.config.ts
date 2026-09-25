29da89fd787c35ecfbfd5f8a8e022b1789da226c
doctor-ops <84721739+doctor-ops@users.noreply.github.com>
2026-09-25 09:08:20 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/gavgavmur2/', // ⬅️ Добавьте эту строку сюда
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
