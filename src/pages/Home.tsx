b4508c04bc5d1e8d95e619d6838adb4cb33af255
doctor-ops <84721739+doctor-ops@users.noreply.github.com>
2026-09-26 08:48:53 +0700

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,31 @@
+import { Hero } from '@/components/Hero';
 import { SmartServices } from '@/components/SmartServices';
+import { Breeds } from '@/components/Breeds';
+import { Care } from '@/components/Care';
+import { FoodTypes } from '@/components/FoodTypes';
+import { Contacts } from '@/components/Contacts';
 
 export function Home() {
   return (
-    <>
+    // Оборачиваем в основной фон, чтобы избежать белых щелей между секциями
+    <div className="bg-base-bg">
+      {/* 1. Первый экран: Эмоциональный захват и главный CTA */}
+      <Hero />
+
+      {/* 2. Быстрый доступ: Инструменты и калькуляторы (Конверсия) */}
       <SmartServices />
-    </>
+
+      {/* 3. Образовательный блок: Каталог пород (Интерес) */}
+      <Breeds />
+
+      {/* 4. Практические советы: Уход за питомцами (Польза) */}
+      <Care />
+
+      {/* 5. Экспертный блок: Питание и корма (Авторитет) */}
+      <FoodTypes />
+
+      {/* 6. Финальный шаг: Обратная связь (Лояльность) */}
+      <Contacts />
+    </div>
   );
 }
