557f5e4f8526d9e36041e343e834854c0bb8280c
doctor-ops <84721739+doctor-ops@users.noreply.github.com>
2026-09-26 09:56:05 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App /> {/* Просто App, без обертки BrowserRouter здесь */}
+    <App />
   </StrictMode>
 );
